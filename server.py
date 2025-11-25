68e2764deea63c39aed49b4596c41101f66ba5de
Jordan Mackenzie <jordantwiddy4@gmail.com>
2025-11-25 21:49:00 +0000

    Added some recv and send packet methods
    Added recv and send packet methods and integrated these into the current
    methods. I also wrote the _send_message logic. Most of the way there
    now.

--- server.py
+++ server.py
@@ -20,6 +20,33 @@ class BeatriceServer:
         # }
         self.connected_users = {}
 
+    async def _receive_packet(self, reader):
+        try:
+            message = await reader.readline()
+            if message == b'': # if an empty string is received, this means the client has disconnected.
+                raise Exception("Client has disconnected. Connection will be closed.")
+            elif not message or message == b'\n':
+                return None # if no message is received or an empty message is received, do nothing
+
+            packet = json.loads(message.decode("utf-8")) # decrypt the utf8 message into JSON packet if its valid json
+            return packet
+        except json.JSONDecodeError: # json error if there is an invalid json
+            return None
+
+
+    async def _send_packet(self, writer, packet):
+        """
+        1. Encodes the packet to JSON bytes (with newline).
+        2. Writes it to the specific writer.
+        3. Drains the writer to ensure it sent.
+        Safety: Wrap in try/except to ignore broken pipes.
+        """
+        try:
+            writer.write((json.dumps(packet) + "\n").encode("utf-8")) # Send the error packet
+            await writer.drain()
+        except Exception: # If an exception occurs
+            pass
+
     async def start_server(self):
         server = await asyncio.start_server(self.handle_client, self.host, self.port)
 
@@ -46,68 +73,57 @@ class BeatriceServer:
 
         Success: Only then do you look at the nickname and let them in.
         """
-        try:
-            message = await asyncio.wait_for(reader.readline(), timeout=5.0)
-            if not message:
+        while True:
+            try:
+                packet = await asyncio.wait_for(self._receive_packet(reader), timeout=5)
+            except Exception:
+                break
+
+            if packet is None:
+                continue
+            elif packet.get("t") == "H":
+
+                # Get the values from the decoded json packet
+                _type = packet.get("t") # Type of packet recieved
+                _nickname = packet.get("n") # Nickname
+                _key = packet.get("k") # Public key
+
+                if not all([_type, _nickname, _key]): # Checks to see that all fields are not none, if any data is missing, send the error packet to the user.
+                    error_msg = "Missing required handshake packet data fields." # Custom error message to send on failure
+                    await self._send_packet(writer, {"t": "ERR", "c": error_msg})
+                    return None
+
+                # Check that the data packet contains the key information.
+                if not _key.startswith("-----BEGIN PUBLIC KEY"):
+                    error_msg = "Invalid public key." # Set custom error message on failure
+                    await self._send_packet(writer, {"t": "ERR", "c": error_msg})
+                    return None
+
+                try:
+                    serialization.load_pem_public_key( _key.encode('utf-8')) # If this passes, the key is mathematically valid
+                except (ValueError, TypeError): # If it does not pass, ValueError is raised, indicating that the public key is invalid.
+                    error_msg = "Invalid public key. Invalid format or encoding. Please provide a valid PEM-encoded public key."
+                    await self._send_packet(writer, {"t": "ERR", "c": error_msg})
+                    return None
+
+                try:
+                    final_nickname = _nickname # Check to make sure this is a nickname that isn't already in use. If it is, append a number and generate another one until you find a non-used name.
+                    if final_nickname in self.connected_users:
+                        suffix = random.randint(100,999)
+                        final_nickname = f"{_nickname}#{suffix}"
+                    self.connected_users[final_nickname] = { # Store this in the connected_users dict
+                        "writer":writer,
+                        "key":_key
+                    }
+                    self.latest_nickname = final_nickname
+                    return final_nickname
+                except Exception:
+                    return None
+
+            else: # If the packet type doesn't begin with "H" which indicates its a handshake request.
+                error_msg = "Error: Received non-handshake packet." # Set custom error message on failure
+                await self._send_packet(writer, {"t": "ERR", "c": error_msg})
                 return None
-        except asyncio.TimeoutError:
-            return None
-
-        try:
-            message_str = message.decode("utf-8")
-        except UnicodeDecodeError:
-            return None
-
-        try:
-            handshake_packet = json.loads(message_str) # decrypt the utf8 message into JSON packet if its valid json
-        except json.JSONDecodeError: # json error if there is an invalid json
-            return None
-
-        # Get the values from the decoded json packet
-        _type = handshake_packet.get("t") # Type of packet recieved
-        _nickname = handshake_packet.get("n") # Nickname
-        _key = handshake_packet.get("k") # Public key
-
-        if not all([_type, _nickname, _key]): # Checks to see that all fields are not none, if any data is missing, send the error packet to the user.
-            error_msg = "Missing required handshake packet data fields." # Custom error message to send on failure
-            writer.write((json.dumps({"t": "ERR", "c": error_msg}) + "\n").encode("utf-8")) # Send the error packet
-            await writer.drain() # Await for the write to drain
-            return None
-
-        if _type != "H": # If the packet type begins with "H" which indicates its a handshake request.
-            error_msg = "Error: Received non-handshake packet." # Set custom error message on failure
-            writer.write((json.dumps({"t": "ERR", "c": error_msg}) + "\n").encode("utf-8")) # Send the error packet
-            await writer.drain() # Await for write to drain
-            return None
-
-        # Check that the data packet contains the key information.
-        if not _key.startswith("-----BEGIN PUBLIC KEY"):
-            error_msg = "Invalid public key." # Set custom error message on failure
-            writer.write((json.dumps({"t": "ERR", "c": error_msg}) + "\n").encode("utf-8")) # Send the error packet
-            await writer.drain() # Await for the write to drain
-            return None
-
-        try:
-            serialization.load_pem_public_key( _key.encode('utf-8')) # If this passes, the key is mathematically valid
-        except (ValueError, TypeError): # If it does not pass, ValueError is raised, indicating that the public key is invalid.
-            error_msg = "Invalid public key. Invalid format or encoding. Please provide a valid PEM-encoded public key."
-            writer.write((json.dumps({"t": "ERR", "c": error_msg}) + "\n").encode("utf-8")) # Send the error packet
-            await writer.drain() # Await for the write to drain
-            return None
-
-        try:
-            final_nickname = _nickname # Check to make sure this is a nickname that isn't already in use. If it is, append a number and generate another one until you find a non-used name.
-            if final_nickname in self.connected_users:
-                suffix = random.randint(100,999)
-                final_nickname = f"{_nickname}#{suffix}"
-            self.connected_users[final_nickname] = { # Store this in the connected_users dict
-                "writer":writer,
-                "key":_key
-            }
-            self.latest_nickname = final_nickname
-            return final_nickname
-        except Exception:
-            return None
 
 
     async def _synchronise(self, writer, new_nickname, new_key):
@@ -118,18 +134,19 @@ class BeatriceServer:
         will need to decode or encode these before sending
         """
 
-        current_users_list = []
+        current_users_list = [] # list of currently connected users, and their public keys. to be sent to new person.
 
-        dir_packet = {
+        dir_packet = { # Current users list packet. To be sent to the new person only.
             "t": "DIR",
             "p": current_users_list
         }
 
+        join_packet = { # Info on the recently joined person, to be sent to everyone else. Bosh
+            "n": new_nickname,
+            "k": new_key
+        }
+
         for user in self.connected_users:
-            join_packet = {
-                "n": new_nickname,
-                "k": new_key
-            }
             if user != new_nickname:
                 try:
                     self.connected_users[user].get("writer").write((json.dumps(join_packet) + "\n").encode('utf-8'))
@@ -138,8 +155,7 @@ class BeatriceServer:
                     pass
 
         try:
-            writer.write((json.dumps(dir_packet) + "\n").encode('utf-8'))
-            await writer.drain()
+            await self._send_packet(writer, dir_packet)
         except Exception:
             pass
 
@@ -150,23 +166,43 @@ class BeatriceServer:
         Make sure that a delimiter is used so the whole message is read properly asyncio.StreamReader()
 
         Handle Disconnection: If a client disconnects, make sure to remove them from connected_users list.
+
+        {
+            "t": "M",          # Type: Message
+            "r": "recipient",  # Who is it for? (e.g., "Bob" or "ALL")
+            "m": "..."         # The Message Content (Encrypted String)
+        }
+
         """
         while True: # continuously listen for incoming messages
-            message = await reader.readline()
-            if message == b'': # if an empty string is received, this means the client has disconnected.
+            try:
+                packet = await self._receive_packet(reader)
+            except Exception:
                 break
-            if not message or message == b'\n': # if no message is received or an empty message is received, do nothing
+
+
+            if packet is None:
                 continue
+            elif packet.get("t") == "M":
+                recipient = packet.get("r")
+                # message = packet.get("m")
+                packet["s"] = nickname
 
-            try:
-                message_str = message.decode("utf-8") # decode the received bytes into a string
-            except UnicodeDecodeError:
-                return None
+                if recipient == "ALL":
+                    for user in self.connected_users:
+                        if user != nickname:
+                            try:
+                                await self._send_packet(self.connected_users[user].get("writer"), packet)  # send to all
+                            except:
+                                pass
+                else:
+                    if recipient in self.connected_users:
+                        if recipient != nickname:
+                            try:
+                                await self._send_packet(self.connected_users[recipient].get("writer"), packet)  # send to all
+                            except:
+                                pass
 
-            try:
-                handshake_packet = json.loads(message_str) # decrypt the utf8 message into JSON packet if its valid json
-            except json.JSONDecodeError: # json error if there is an invalid json
-                return None
 
     async def _cleanup(self, nickname, writer):
         """
